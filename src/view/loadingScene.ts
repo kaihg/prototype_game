4a414cec7329de248520b7a478f65df004e40a2c
KK <kaihghuang@simbo.com.tw>
2020-06-10 15:16:48 +0800

    Add the init loading Scene

---
+++ src/view/loadingScene.ts
@@ -0,0 +1,20 @@
+export class LoadingScene extends Phaser.Scene {
+
+    private info: Phaser.GameObjects.Text;
+    private arrow;
+
+    constructor() {
+        super({ "key": "LoadingScene" })
+        console.log('do i start?')
+    }
+
+    create(): void {
+        console.log('do i start? on create')
+        this.info = this.add.text(20, 20, "test", { font: '24px Arial Bold', fill: '#FBFBAC' });
+        this.arrow = this.add.image(200,200,"arrow_d");
+    }
+
+    preload(): void {
+        this.load.image("arrow_d", 'assets/arrow_d.png');
+    }
+}
